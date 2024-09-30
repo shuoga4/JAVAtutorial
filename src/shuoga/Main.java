728800936c8d7c4f210ab8e6fbbc0b49a8de38d6
shuoga <shuoga0426@gmail.com>
2024-09-30 19:46:32 +0900

    datatype learning

---
+++ src/shuoga/Main.java
@@ -0,0 +1,16 @@
+package shuoga;
+
+public class Main {
+    public static void main(String[] args) {
+        // here is important for now
+        System.out.println("Hello world!CXVXCVXCVxCVaoeiaoei");
+
+        /*
+        datatype name_of_variable; // Declaration
+        datatype name_of_variable = value_of_variable; // Initialization
+        datatyp.
+
+         */
+
+    }
+}
