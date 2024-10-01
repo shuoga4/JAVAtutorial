9cb0338ee713eb291b98d5696e5b0386834ccc42
shuoga <shuoga0426@gmail.com>
2024-10-01 20:15:10 +0900

    Input Output learned

--- src/shuoga/Main.java
+++ src/shuoga/Main.java
@@ -1,16 +1,45 @@
 package shuoga;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // here is important for now
-        System.out.println("Hello world!CXVXCVXCVxCVaoeiaoei");
+        System.out.println("Hello world!");
 
-        /*
+        /*      java part 2
         datatype name_of_variable; // Declaration
         datatype name_of_variable = value_of_variable; // Initialization
-        datatyp.
+        (assignment of value)
+        datatype
+        int
+        float (number end f)
+        double
+        boolean
+        char 'single character' ex A
+        string "multiple character" ex hello
+
+
 
          */
+
+        /*  java part 4
+            input and output
+
+
+            Output    */
+
+        System.out.println("aoeiaoeiaoeiaoeiaoei");
+        int age = 21;
+        System.out.println("my age is " + age);
+
+        /* Input   */
+
+        Scanner shuogascanner = new Scanner(System.in);
+        String stringinput = shuogascanner.next();
+        System.out.println(stringinput + " was typed");
+
 
     }
 }
