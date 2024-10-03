46c753174e5bc76e3c2598a160f9a59aab775b2e
shuoga <shuoga0426@gmail.com>
2024-10-03 19:31:22 +0900

    math method learned

--- src/shuoga/MainPart5.java
+++ src/shuoga/MainPart5.java
@@ -16,11 +16,39 @@ public class MainPart5 {
 
         System.out.println("absolute : " + Math.abs(-1000));
 
-        System.out.println("ceiling : " + Math.ceil(1.6453425)); // 1.34 = 2.0
 
-        System.out.println("round : " + Math.round(1.6453425)); // 1.34 = 1
 
-        System.out.println("floor : " + Math.floor(1.6453425));
+        System.out.println("ceiling : " + Math.ceil(1.6453425)); // 1.34 = 2.0 ,1.64 = 2.0 rounding up
+
+        System.out.println("round : " + Math.round(1.6453425)); // 1.34 = 1 ,1.64 = 2 round
+
+        System.out.println("floor : " + Math.floor(1.6453425)); // 1.34 = 1, 1.64 = 1 rounding down
+
+
+
+        System.out.println("max : " + Math.max(25,56));
+
+        System.out.println("min : " + Math.min(6,7));
+
+
+        // a little bit of learning math method
+
+        System.out.println("absExact : " + Math.absExact(-324234234));
+
+        System.out.println("cos(0) : " + Math.cos(0));
+
+        System.out.println("ulp : " + Math.ulp(1));
+
+
+        /*
+        booleans and logic
+        comparison (< > <= >= == !=)
+         */
+
+
+
+
+
 
 
     }
