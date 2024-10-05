987d06dfe521c7007530219210a4cc1da6d60b2f
shuoga <shuoga0426@gmail.com>
2024-10-05 19:45:38 +0900

    if learned

--- src/shuoga/Mainpart6.java
+++ src/shuoga/Mainpart6.java
@@ -1,5 +1,8 @@
 package shuoga;
 
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
 public class Mainpart6 {
     public static void main(String[] args) {
         System.out.println("hello guys");
@@ -11,6 +14,43 @@ public class Mainpart6 {
         // == > < <= >= !=
 
         //Assignment =
+        /*
+        variable += 100 ( variable = variable + 100)
+        variable ++
+        variable -=
+        variable --
+        variable *=
+        variable /=
+        variable %=
+
+         */
+
+
+
+
+        /*
+        control flow (if and switch)
+
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("from 0 to 99, what's your favorite number?");
+        int favnum = scanner.nextInt();
+        double random = Math.random();
+        random *= 100;
+        random = Math.floor(random);
+
+        if (favnum == (int)random){
+            System.out.println("you did it!");
+        }else if (favnum > (int)random){
+            System.out.println("you are bigger");
+            System.out.println("from 0 to 99, what's your favorite number?");
+        }else {
+            System.out.println("you are smaller");
+            System.out.println("from 0 to 99, what's your favorite number?");
+        }
+
+
 
 
     }
