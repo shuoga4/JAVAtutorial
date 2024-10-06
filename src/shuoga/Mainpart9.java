b1360b19c80bff2179fd6b88ea1f29979a6f966a
shuoga <shuoga0426@gmail.com>
2024-10-06 19:12:59 +0900

    for loop learned

---
+++ src/shuoga/Mainpart9.java
@@ -0,0 +1,45 @@
+package shuoga;
+
+import java.util.Scanner;
+
+public class Mainpart9 {
+    public static void main(String[] args) {
+
+
+        // For and While loop
+
+        //For
+
+
+        for(int i = 0; i == 0; i++)
+            System.out.println(i);
+
+
+        // count hitter but use for loop
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("from 0 to 99, what's your favorite number?");
+        int favnum = scanner.nextInt();
+        double random = Math.random();
+        random *= 100;
+        random = Math.floor(random);
+
+        for (int i = 0; i == 0;){
+            if (favnum == (int)random){
+                System.out.println("you did it!");
+                i = 1;
+                continue;
+            }else if (favnum > (int)random){
+                System.out.println("you are bigger");
+            }else {
+                System.out.println("you are smaller");
+            }
+            System.out.println("try again!");
+            favnum = scanner.nextInt();
+        }
+
+
+    }
+
+}
