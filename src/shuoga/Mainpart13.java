bffcaee6c86ee8f5fc15b9413e43e52f5af295b1
shuoga <shuoga0426@gmail.com>
2024-10-12 18:27:21 +0900

    string?

---
+++ src/shuoga/Mainpart13.java
@@ -0,0 +1,12 @@
+package shuoga;
+
+public class Mainpart13 {
+    public static void main(String[] args) {
+        String shuoga = "shuoga";
+        System.out.println("My name is " + shuoga);
+        char first = shuoga.charAt(0);
+        System.out.println(first);
+
+        
+    }
+}
