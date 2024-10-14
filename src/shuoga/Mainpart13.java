92fc9c38d18c85d69a188f686b485502edbeb7d7
shuoga <shuoga0426@gmail.com>
2024-10-14 11:12:17 +0900

    more regular expression

--- src/shuoga/Mainpart13.java
+++ src/shuoga/Mainpart13.java
@@ -2,6 +2,8 @@ package shuoga;
 
 // https://magazine.techacademy.jp/magazine/28637
 // Regular Expression
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Mainpart13 {
     public static void main(String[] args) {
@@ -16,4 +18,18 @@ public class Mainpart13 {
 
         
     }
+
+// regular Expression
+    public static class Mainpart132 {
+        public static void main(String[] args) {
+            String shuoga = "shuoga0426";
+            System.out.println(shuoga.matches(".*[0-9]*")); // 任意文字n回後0-9n回と完全一致
+            Pattern birthday = Pattern.compile(".*[0-9]*");
+            Matcher M = birthday.matcher(shuoga);
+            System.out.println(M.find());
+
+
+        }
+    }
+
 }
