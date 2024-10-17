9000e1ad216cf61c857c165afc824d5401dd286d
shuoga <shuoga0426@gmail.com>
2024-10-17 19:41:01 +0900

    siritori is not under construction

--- src/shuoga/Mainpart14.java
+++ src/shuoga/Mainpart14.java
@@ -1,19 +1,19 @@
 package shuoga;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Mainpart14 {
     //しりとりゲーム
     public static void main(String[] args) {
         // Set使う
-        Set<String> siritori = new HashSet<>();
+        List<String> siritori = new ArrayList<>();
         System.out.println("Say the first word : ");
         Scanner scanner = new Scanner(System.in);
         String myword = scanner.next();
         // 正規表現で次の言葉としりとりできてるか判定
         // 正規表現で謎の文字が入ってないか確認（主に数字)
-        //
+        // 単語検索機能？
+
+
     }
 }
