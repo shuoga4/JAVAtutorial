6547e0a13d98590cf88e1cf7621a98bcb4675336
shuoga <shuoga0426@gmail.com>
2024-10-21 12:21:57 +0900

    more regular expression

--- src/shuoga/Mainpart15.java
+++ src/shuoga/Mainpart15.java
@@ -7,10 +7,10 @@ import java.util.regex.Pattern;
 public class Mainpart15 {
     public static void main(String[] args) {
         //判定する文字列
-        String str = "012-345-6789";
+        String str = "aieXeaei324-2344a";
         //判定するパターンを生成
         // Pattern p = Pattern.compile("\\d{2,4}-\\d{2,4}-\\d{4}"); //電話番号
-        Pattern p = Pattern.compile("^\\d{3}-\\d{4}$"); //郵便番号
+        Pattern p = Pattern.compile("^[XYZ]+.*\\d{3}-\\d{4}"); //郵便番号
         Matcher m = p.matcher(str);
 
         System.out.println(m.find()); //true
