09fe4fa95cf9412bb2f2037015648c10fa379cd6
shuoga <shuoga0426@gmail.com>
2024-10-27 20:29:04 +0900

    class calling

--- src/shuoga/Mainpart17.java
+++ src/shuoga/Mainpart17.java
@@ -2,20 +2,36 @@ package shuoga;
 
 import java.util.Scanner;
 
-class LogicPart{
-    private double A,B;
-    private int operation;
-    LogicPart(double A, double B,int operation){
+class Calc{
+    private final double A,B;
+    private double sum;
+    private final int operation;
+
+    Calc(double A, double B,int operation){
         this.A = A;
         this.B = B;
         this.operation = operation;
     }
+    public double calc (){
+        switch (operation){
+            case 1: sum = A + B;break;
+            case 2: sum = A - B;break;
+            case 3: sum = A * B;break;
+            case 4:
+                if (B == 0)sum = 0;
+                else sum = A / B;
+                break;
+        }
+        return sum;
+    }
+
 
 }
 
 public class Mainpart17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("1:+ , 2:- , 3:* , 4:/");
         int operation = scanner.nextInt();
         System.out.println("type number A");
@@ -23,8 +39,8 @@ public class Mainpart17 {
         System.out.println("type number B");
         float variable_B = scanner.nextFloat();
 
-        // logic
+        Calc calc = new Calc(variable_A,variable_B, operation);
+        System.out.println("the answer is : " + calc.calc());
 
-        System.out.println();
     }
 }
