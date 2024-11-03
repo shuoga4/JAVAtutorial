54b50f2d12fca1cb0be40418574038fba43bddc3
shuoga <shuoga0426@gmail.com>
2024-11-03 12:34:08 +0900

    public private and what is protected?

---
+++ src/shuoga2/Mainpart18.java
@@ -0,0 +1,47 @@
+package shuoga2;
+
+class Human{
+    private String name = "noname";
+    private int health = 50;
+    private int atk = 10;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getAtk(){
+        return this.atk;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public void setAtk(int atk){
+        this.atk = atk;
+    }
+
+
+    public void attack(Human human){
+        int hp = human.getHealth();
+        hp = hp - this.atk;
+        human.setHealth(hp);
+    }
+}
+
+// require:
+// input class
+// data class
+// what is protected
+
+
+public class Mainpart18 {
+}
