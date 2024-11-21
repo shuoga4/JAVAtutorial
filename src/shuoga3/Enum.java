fd80b5df9537fce7c00149afcab9f6fc60addf5d
shuoga <shuoga0426@gmail.com>
2024-11-21 18:43:28 +0900

    enum!

--- src/shuoga3/Enum.java
+++ src/shuoga3/Enum.java
@@ -1,4 +1,25 @@
 package shuoga3;
 
+
 public enum Enum {
+
+
+
+    CheckRelationShipRate("main","sub"),
+    CheckRelationShipChoose("hate", "like");
+
+    private String mainCharacter;
+    private String mainHeroine;
+    private String hate;
+    private String like;
+
+    Enum(String main,String sub){
+        this.mainCharacter = main;
+        this.mainHeroine = sub;
+    }
+
+
+
+
+
 }
