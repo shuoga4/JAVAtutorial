95f74ac54dd0fe1c2b47d99b2f5b05c4fc30f3c5
shuoga <shuoga0426@gmail.com>
2024-12-02 18:51:38 +0900

    Streams and lambda2!

--- src/shuoga7/Mainpart28.java
+++ src/shuoga7/Mainpart28.java
@@ -1,4 +1,49 @@
 package shuoga7;
 
+import java.util.function.Function;
+
 public class Mainpart28 {
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                helloWorld();
+            }
+        };
+
+        Runnable runnable1 = () -> System.out.println("hello!");
+        runAnything(runnable);
+        runAnything(runnable1);
+
+        System.out.println("------------------------------------------");
+
+        Function<String, String> hello = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return "hello! : " + s;
+            }
+        };
+
+        Function<String,String> hello2 = (x) -> "hello2 : " + x;
+
+        System.out.println(hello.apply("aloe"));
+        System.out.println(hello2.apply("aloe2"));
+
+        System.out.println("------------------------------------------");
+
+        //runnableは、パラメータなしでもいいこと、ラムダ式の処理部分が実行形式なこと
+        //Functionは、引数の型と戻り値の型の指定をすること、パラメータ必須なこと、処理は戻り値を書くこと
+        //定義そのままじゃん、ラナブル＝実行可能、ファンクション＝関数＝xいれてyだす
+
+
+    }
+    public static void helloWorld(){
+        System.out.println("hello");
+    }
+
+    public static void runAnything(Runnable runnable){
+        runnable.run();
+    }
+
 }
